638373e1dc57c9e335be6051b7e4d9b68877a4b7
show <show75@ezweb.ne.jp>
2020-01-11 18:02:10 +0900

    Add 3 Action create & update & destroy

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,14 +1,47 @@
+module Api
+    module V1        
+        class TasksController < ApplicationController
+            # GET /api/v1/tasks
+            def index
+                @tasks = Task.all.order(id: "DESC")
+                render json: { status: 'SUCCESS', data: @tasks } 
+            end
 
-class Api::V1::TasksController < ApplicationController
-    # GET /api/v1/tasks
-    def index
-        @tasks = Task.all.order(id: "DESC")
-        render json: @tasks
-    end
+            # POST /api/v1/tasks
+            def create
+                @task = Task.new(task_params)
+                
+                if @task.save
+                    render json: { status: 'SUCCESS', data: @task }
+                else
+                    render json: { status: 'ERROR', data: @task.errors }
+                end
+            end
 
-    # POST /api/v1/tasks
-    def create
-        @task = Taks.new(title: params[:title])
-    end
+            # PATCH /api/v1/tasks/:id
+            def update
+                @task = Task.find(params[:id])
+                @task.title = params[:title]
+
+                if @task.save
+                    render json: { status: 'SUCCESS', data: @task }
+                else
+                    render json: { status: 'ERROR', data: @task.errors }
+                end
+            end
+
+            # DELETE /api/vi/tasks/:id
+            def destroy
+                @task = Task.find(params[:id])
+                @task.destroy
+                render json: { status: 'SUCCESS', data: @task }
+            end
+
+            private 
+            
+            def task_params
+                params.require(:task).permit(:title)
+            end
+        end
+     end
 end
-
