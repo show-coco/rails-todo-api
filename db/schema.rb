ed44cce678087ecb8349bf2b4bc317b26da09876
show <show75@ezweb.ne.jp>
2020-01-12 21:49:14 +0900

    Add period-column To tasks-table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_12_040538) do
+ActiveRecord::Schema.define(version: 2020_01_12_124145) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.boolean "is_done", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "period"
   end
 
   create_table "users", force: :cascade do |t|
